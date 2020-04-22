c6ef7a303d743c8e5749cab1fac4601edcec52b4
CristinaPerez96 <62713997+CristinaPerez96@users.noreply.github.com>
2020-04-23 00:35:23 +0200

    version 3 generado por Cristina Perez

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -46,42 +46,46 @@
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.button1.Location = new System.Drawing.Point(12, 22);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(103, 47);
             this.button1.TabIndex = 0;
             this.button1.Text = "Conectar";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
+            this.button2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.button2.Location = new System.Drawing.Point(179, 22);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(103, 47);
             this.button2.TabIndex = 1;
             this.button2.Text = "Desconectar";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
+            this.button3.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.button3.Location = new System.Drawing.Point(31, 228);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(103, 44);
             this.button3.TabIndex = 2;
             this.button3.Text = "Registrarse";
-            this.button3.UseVisualStyleBackColor = true;
+            this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
+            this.button4.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.button4.Location = new System.Drawing.Point(179, 228);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(103, 44);
             this.button4.TabIndex = 3;
             this.button4.Text = "Iniciar sesión";
-            this.button4.UseVisualStyleBackColor = true;
+            this.button4.UseVisualStyleBackColor = false;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // label1
@@ -160,7 +164,7 @@
             this.dataGridView1.Location = new System.Drawing.Point(528, 43);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(240, 356);
+            this.dataGridView1.Size = new System.Drawing.Size(218, 356);
             this.dataGridView1.TabIndex = 13;
             // 
             // Jugador
